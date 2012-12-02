83a3b464746b36d9a12d7efbd381cbec42db95d7
Matias Saarinen <matias.saarinen@aalto.fi>
2012-12-02 14:19:15 +0200

    palautuskuntoon

--- src/Apu.java
+++
@@ -1,9 +0,0 @@
-
-public class Apu {
-	public static double positiivinen( double luku ) {
-		return (luku < 0) ? -luku : luku;
-	}
-	public static int positiivinenInt( int luku ) {
-		return (int) positiivinen( luku );
-	}
-}
--- src/NappiKuuntelija.java
+++
@@ -1,4 +0,0 @@
-
-////public interface NappiKuuntelija {
-//	public void nappiMuuttunut( MiinaharavaRuutu ruutu );
-//}
